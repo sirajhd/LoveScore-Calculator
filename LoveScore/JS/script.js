b60332b6e4dcee37db19167426820d2aa87a45cc
Siraj Hasseno <sirajdalu@gmail.com>
2024-12-03 12:37:29 -0800

    Add files via upload

---
+++ LoveScore/JS/script.js
@@ -0,0 +1,18 @@
+// var myname = prompt("please enter your name");
+// while (myname.trim() === "") {
+//     myname = prompt("Name can't be empty. Please enter your name");
+// }
+
+// var theirName = prompt("please enter their name");
+// while (theirName.trim() === "") {
+//     theirName = prompt("Name can't be empty. Please enter their name");
+// }
+
+// var loveScore=Math.random()*100;
+// loveScore=Math.floor(loveScore)+1;
+// if(loveScore>=50){
+//     alert("dear "+myname+" your loveScore with "+theirName+" is "+loveScore+"%")
+// }else{
+//     alert("dear "+myname+" your loveScore with "+theirName+" is "+loveScore+"%")
+
+// }
